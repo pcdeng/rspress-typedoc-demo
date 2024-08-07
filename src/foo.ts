1f275d68750cf00c9602f361d6615f7b5f98e95f
Payton <565524110@qq.com>
2024-08-07 18:00:37 +0800

    feat: add typescript file

---
+++ src/foo.ts
@@ -0,0 +1,15 @@
+/**
+ * 这是一个add函数
+ */
+export function add(
+  /**
+   * This is param1
+   */
+  param1: string,
+  /**
+   * This is param2
+   */
+  param2: number
+) {
+  return param1 + param2;
+}
